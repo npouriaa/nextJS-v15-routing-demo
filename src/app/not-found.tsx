9d2ba13731412dcf794794466db4bb09fcdedae1
Pouria Navipour <pouria.navipour@varexe.com>
2025-02-15 13:38:35 +0330

    add 404 - not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-screen h-screen absolute top-0 left-0 bg-red-500 flex items-center justify-center">
+        <h1 className="font-bold text-3xl">Page Not Found | 404</h1>
+    </div>
+  )
+}
+
+export default NotFound
