6539ce16f2b3ed585e12df31dbcc20a0ebdd7642
Pouria Navipour <pouria.navipour@varexe.com>
2025-02-16 18:00:09 +0330

    move header and footer code to seperate files

---
+++ src/components/layouts/root/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="p-4 bg-gray-400">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layouts/root/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="p-4 bg-blue-400">
+      <p>Header</p>
+    </header>
+  );
+};
+
+export default Header;
