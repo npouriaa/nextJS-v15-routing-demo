19cd3b61b22eed5750f7d1e6138f4a004f1e53b3
Pouria Navipour <npouriaa@gmail.com>
2025-02-18 12:53:54 +0330

    Loading UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,19 @@ export const metadata = {
   title: "Blog",
 };
 
-const Blog = () => {
+const Blog = async () => {
+  // real API call delay
+  // await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  //   .then((res) => res.json())
+  //   .then((result) => console.log(result));
+
+  // Intentional delay
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional delay");
+    }, 2000);
+  });
+
   return <h1>Blog page</h1>;
 };
 
